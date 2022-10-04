2e3c68f426b60705639cf77e5291a7595046b6d2
hupe1980 <frankhuebner1980@gmail.com>
2022-10-04 22:13:25 +0200

    Misc

--- pkg/http/http_test.go
+++ pkg/http/http_test.go
@@ -9,5 +9,5 @@ import (
 func TestPayload(t *testing.T) {
 	http := NewHTTP()
 
-	assert.Equal(t, "gopher://127.0.0.1:80/_GET%20/%20HTTP/1.0%0A%0A", http.Payload())
+	assert.Equal(t, "gopher://127.0.0.1:80/_GET%20%2F%20HTTP/1.0%0D%0AHost:%20127.0.0.1:80%0D%0AUser-Agent:%20gopherfy%0D%0AAccept:%20*/*%0D%0A", http.Payload())
 }
